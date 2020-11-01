199e12be0b02f289e78be1956fe1495a1689eacd
Yusuke Watanabe <you-sk@pc.email.ne.jp>
2020-11-01 11:35:15 +0900

    1st commit

---
+++ Assets/RocketController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-0.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(0.1f, 0, 0);
+        }
+    }
+}
